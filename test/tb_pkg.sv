08fbc7516f4b6e9ff73c27f28a6eb695588d492f
Vikas K <83337729+Vk13io@users.noreply.github.com>
2025-04-01 17:58:48 +0530

    created tb_pkg.sv

---
+++ test/tb_pkg.sv
@@ -0,0 +1,17 @@
+
+`include "uvm_macros.svh"
+package tb_pkg;
+ import uvm_pkg::*;
+ `include "ring_sequence_item.sv"  
+ `include "ring_sequence.sv"             
+ `include "ring_sequencer.sv"           
+ `include "ring_driver.sv"              
+ `include "ring_monitor.sv"            
+ `include "ring_agent.sv"                         
+ `include "ring_scoreboard.sv"        
+ `include "ring_env.sv"                
+ `include "ring_test.sv"                
+
+endpackage
+
+
